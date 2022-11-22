1e4043da0d0f057941495071fb546113723586e0
emapodhorna <ema.podhorna@gmail.com>
2022-11-22 13:37:02 +0100

    uz to vypisuje aj viac argumentov

--- arxiv_bibdata.sh
+++ arxiv_bibdata.sh
@@ -2,14 +2,12 @@
 
 PREM="https://arxiv.org/abs/"
 
-while [ "$#" != 0 ]
+for ARG in "$@"
 do
-	for x in "$@"
-	do
-		PREM="$PREM$x"
-		curl -s "$PREM" | tr -d '\n' | tr '<' '\n' | grep 'meta name="citation' | sed 's/" content=/=/' | sed 's/meta name="citation_//' | sed 's/\/>//' 
-	done
-shift
+	PREM="$PREM$ARG"
+	curl -s "$PREM" | tr -d '\n' | tr '<' '\n' | grep 'meta name="citation_' | sed 's/" content=/=/' | sed 's/meta name="citation_//' | sed 's/\/>//' 
+	PREM="https://arxiv.org/abs/"
+	shift
 done
 
 
